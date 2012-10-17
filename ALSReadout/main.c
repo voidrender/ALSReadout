576c515f558db3a2eb2d4929361d340bf12a897a
Isaac Overacker <electricskyline@gmail.com>
2012-10-16 17:33:28 -0700

    Moved declaration outside of loop.

--- ALSReadout/main.c
+++ ALSReadout/main.c
@@ -16,14 +16,15 @@ int main(int argc, const char * argv[])
     io_connect_t port = 0;
     IOServiceOpen(service, mach_task_self(), 0, &port);
 
+    uint32_t outputs = 2;
+    uint64_t values[outputs];
+
     while (true)
     {
         sleep(1);
-        uint32_t outputs = 2;
-        uint64_t values[outputs];
 
         // Output values will be on the interval [0, 67092480]
-        IOConnectCallMethod(port, 0, nil, 0, nil, 0, values , &outputs, nil, 0);
+        IOConnectCallMethod(port, 0, nil, 0, nil, 0, values, &outputs, nil, 0);
 
         printf("%lld, %lld\n", values[0], values[1]);
 
